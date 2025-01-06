a32932cf0bcfad59d2372fc33bc1b59794df8851
Jaros <jaros@chimplie.com>
2025-01-06 13:18:17 +0100

    add links formatting and styles

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const saveBtn = document.getElementById("input-btn");
 saveBtn.addEventListener("click", function() {
     saveInput()
     renderLinks()
+    console.log(myLinks);
 });
 
 // INPUT
@@ -12,20 +13,21 @@ const inputFld = document.getElementById("input-el");
 
 function saveInput() {
     myLinks.push(inputFld.value);
-    console.log(myLinks);
+    inputFld.value = "";
 }
 
 // OUTPUT
 const outputLst = document.getElementById("ul-el");
 
-
 function renderLinks() {
     let listItems = "";
-
-    for (let i = 0; i < myLinks.length; i++) {    
-        listItems += "<li>" + myLinks[i] + "</li>";
+    for (let i = 0; i < myLinks.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href="${myLinks[i]}">${myLinks[i]}</a>
+            </li>
+        `
     }
-
     outputLst.innerHTML = listItems
 }
 
